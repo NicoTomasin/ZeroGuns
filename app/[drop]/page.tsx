dc0f838484ec61d1f865d0ac56fca85cb05ebac4
NicoTomasin <nicolasgabrieltomasin@gmail.com>
2024-02-09 03:48:13 -0300

    try to fix color dinamic in ssg

--- app/[drop]/page.tsx
+++ app/[drop]/page.tsx
@@ -9,28 +9,30 @@ import { GetStaticPaths } from "next";
 export default async function Drop({ params }: { params: { drop: string } }) {
   const filePath = path.join(process.cwd(), "public", "data.json");
   const data = await fs.readFile(filePath, "utf8");
-  const drops = JSON.parse(data);
-  if (!drops[params.drop]) {
-    redirect("/404");
-  }
-  const { color, borderColor, title, subtitle } = drops[params.drop];
+  const dataParsed = JSON.parse(data);
+  const drop = dataParsed[params.drop];
+  const { title, subtitle, color, borderColor } = drop;
   return (
-    <div
-      className={`min-h-screen px-4 py-12 min-w-screen flex flex-col justify-center items-center ${color}`}
-    >
-      <Title title={title} borderColor={borderColor} />
-      <Subtitle subtitle={subtitle} />
-      <Logo />
-      <Media video={`/media/${params.drop}.mp4`} />
-    </div>
+    <>
+      <div
+        className={
+          "min-h-screen px-4 py-12 min-w-screen flex flex-col justify-center items-center " +
+          color
+        }
+      >
+        <Title title={title} borderColor={borderColor} />
+        <Subtitle subtitle={subtitle} />
+        <Logo />
+        <Media video={`/media/${params.drop}.mp4`} />
+      </div>
+    </>
   );
 }
 export async function generateStaticParams() {
   const filePath = path.join(process.cwd(), "public", "data.json");
   const data = await fs.readFile(filePath, "utf8");
-  const drops = JSON.parse(data);
-
-  return Object.keys(drops).map((drop) => {
-    drop: drop;
-  });
+  const dataParsed = JSON.parse(data);
+  return dataParsed.drops.map((drop: string) => ({
+    drop,
+  }));
 }
